d7c91b6342b46c70ec59dd0913fbdfcb277a9a44
Elkin Guerrero <elkincrf.dev@gmail.com>
2022-10-31 08:59:17 -0500

    file pdf 3

--- pdfFileSrc.js
+++ pdfFileSrc.js
@@ -1,6 +1,5 @@
-const srcPdfFile = document.getElementsByClassName('src-pdf-file');
+// const srcPdfFile = document.getElementsByClassName('src-pdf-file');
 const btnDownloadCV = document.getElementsByClassName('card-header-a__button-cv__btn');
-
-// console.log(srcPdfFile[0].src)
 btnDownloadCV[0].href = '/assets/elkin-cv.d8898e81.pdf';
-console.log(btnDownloadCV[0].href)
+// console.log(btnDownloadCV[0].href)
+//the img useing the pdf file with that we get in build and i see the has after i add to the DOM
