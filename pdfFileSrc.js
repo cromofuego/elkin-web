e22afcda5fe794e95ea4a633ddd0a3a11bf8aa8e
Elkin Guerrero <elkincrf.dev@gmail.com>
2022-10-31 08:56:23 -0500

    pdf file 2

---
+++ pdfFileSrc.js
@@ -0,0 +1,6 @@
+const srcPdfFile = document.getElementsByClassName('src-pdf-file');
+const btnDownloadCV = document.getElementsByClassName('card-header-a__button-cv__btn');
+
+// console.log(srcPdfFile[0].src)
+btnDownloadCV[0].href = '/assets/elkin-cv.d8898e81.pdf';
+console.log(btnDownloadCV[0].href)
